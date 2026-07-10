3e4a55ef15d7499ebdcc01dc731d96c1e0da60b9
dternosky0709 <2dhytbn6cc@privaterelay.appleid.com>
2026-07-10 12:09:56 -0400

    Load Sprint 8 work order packet release

--- release.js
+++ release.js
@@ -1,4 +1,4 @@
-const RELEASE_VERSION = "v0.8.3";
+const RELEASE_VERSION = "v0.9.0-alpha1";
 
 function applyReleaseVersionToActiveWalk() {
   try {
@@ -15,6 +15,24 @@ function applyReleaseVersionToActiveWalk() {
   }
 }
 
+function loadSprint8Assets() {
+  if (!document.querySelector('link[data-sprint8="true"]')) {
+    const stylesheet = document.createElement("link");
+    stylesheet.rel = "stylesheet";
+    stylesheet.href = "sprint8.css?v=0.9.0-alpha1";
+    stylesheet.dataset.sprint8 = "true";
+    document.head.appendChild(stylesheet);
+  }
+
+  if (!document.querySelector('script[data-sprint8="true"]')) {
+    const script = document.createElement("script");
+    script.src = "sprint8.js?v=0.9.0-alpha1";
+    script.dataset.sprint8 = "true";
+    script.defer = true;
+    document.body.appendChild(script);
+  }
+}
+
 const releaseStartButton = document.getElementById("startWalkBtn");
 if (releaseStartButton) {
   releaseStartButton.addEventListener("click", () => {
@@ -22,4 +40,5 @@ if (releaseStartButton) {
   });
 }
 
+loadSprint8Assets();
 window.setTimeout(applyReleaseVersionToActiveWalk, 500);
