7a73419757c44541e6a4a7393cf820c359e04e62
dternosky0709 <2dhytbn6cc@privaterelay.appleid.com>
2026-07-10 12:37:22 -0400

    Promote Sprint 8 Alpha 2 assets

--- release.js
+++ release.js
@@ -1,4 +1,4 @@
-const RELEASE_VERSION = "v0.9.0-alpha1";
+const RELEASE_VERSION = "v0.9.1-alpha2";
 
 function applyReleaseVersionToActiveWalk() {
   try {
@@ -19,14 +19,14 @@ function loadSprint8Assets() {
   if (!document.querySelector('link[data-sprint8="true"]')) {
     const stylesheet = document.createElement("link");
     stylesheet.rel = "stylesheet";
-    stylesheet.href = "sprint8.css?v=0.9.0-alpha1";
+    stylesheet.href = "sprint8.css?v=0.9.1-alpha2";
     stylesheet.dataset.sprint8 = "true";
     document.head.appendChild(stylesheet);
   }
 
   if (!document.querySelector('script[data-sprint8="true"]')) {
     const script = document.createElement("script");
-    script.src = "sprint8.js?v=0.9.0-alpha1";
+    script.src = "sprint8.js?v=0.9.1-alpha2";
     script.dataset.sprint8 = "true";
     script.defer = true;
     document.body.appendChild(script);
